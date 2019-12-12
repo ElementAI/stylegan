ca3570812e6a71725df897f408113514eb5e6165
tboquet <thomas.boquet@hec.ca>
2019-12-12 01:43:06 -0500

    * typo

--- dataset_tool.py
+++ dataset_tool.py
@@ -721,7 +721,7 @@ def create_from_hdf5_ssense(tfrecord_dir, hdf5_filename, shuffle):
             order = tfr.choose_shuffled_order() if shuffle else np.arange(
                 hdf5_image.shape[0])
             for idx in range(order.size):
-                shuffled_im = np.tranpose(hdf5_image[order[idx]], (2, 0, 1))
+                shuffled_im = np.transpose(hdf5_image[order[idx]], (2, 0, 1))
                 tfr.add_image(shuffled_im)
                 category = translate_cat(hdf5_category[order[idx]])
                 tfr.add_labels(category)
